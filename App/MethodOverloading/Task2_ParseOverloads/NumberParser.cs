30c387b29be2a99fa38fa6408bac19db7dedeaf2
user1 <user1@DESKTOP-UNR49SD>
2025-09-13 12:57:15 +0300

    решение задачи 2

--- App/MethodOverloading/Task2_ParseOverloads/NumberParser.cs
+++ App/MethodOverloading/Task2_ParseOverloads/NumberParser.cs
@@ -13,13 +13,74 @@ public static class NumberParser
     /// Разбирает строку в int. При неверном формате должен кидать FormatException
     public static int Parse(string s)
     {
-        throw new NotImplementedException();
+        if (s == null)
+            throw new ArgumentNullException(nameof(s), "Строка не может быть null");
+
+        s = s.Trim();
+
+        if (string.IsNullOrEmpty(s))
+            throw new FormatException("Строка не может быть пустой или содержать только пробелы");
+
+        int startIndex = 0;
+        int sign = 1;
+
+        if (s.Length > 0)
+        {
+            if (s[0] == '+')
+            {
+                startIndex = 1;
+            }
+            else if (s[0] == '-')
+            {
+                startIndex = 1;
+                sign = -1;
+            }
+        }
+
+        if (startIndex >= s.Length)
+            throw new FormatException("Строка должна содержать цифры после знака");
+
+        long result = 0;
+
+        for (int i = startIndex; i < s.Length; i++)
+        {
+            char c = s[i];
+
+            if (c < '0' || c > '9')
+                throw new FormatException($"Недопустимый символ '{c}' в позиции {i}");
+
+            int digit = c - '0';
+            result = result * 10 + digit;
+
+            if (sign == 1 && result > int.MaxValue)
+                throw new OverflowException("Число превышает максимальное значение int");
+            if (sign == -1 && result > -(long)int.MinValue)
+                throw new OverflowException("Число меньше минимального значения int");
+        }
+
+        return (int)result * sign;
+    
     }
 
     /// Разбирает строку в int. При неверном формате возвращает defaultValue
     public static int Parse(string s, int defaultValue)
     {
-        throw new NotImplementedException();
+        try
+        {
+            return Parse(s);
+        }
+        catch (ArgumentNullException)
+        {
+            return defaultValue;
+        }
+        catch (FormatException)
+        {
+            return defaultValue;
+        }
+        catch (OverflowException)
+        {
+            return defaultValue;
+        }
     }
 
     /// Разбирает строку с учётом стиля и культуры
