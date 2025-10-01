342835273f2708fad4bff10dfe30b231cd1593c9
user1 <user1@DESKTOP-UNR49SD>
2025-10-01 11:08:41 +0300

    Revert "Task1_SimpleDictionary кт"
    This reverts commit aa82ee9391e72ebdcf16710524ca9dfed3bafe71.

--- App/IndexerOverloading/Task1_SimpleDictionary/MultiKeyDictionary.cs
+++ App/IndexerOverloading/Task1_SimpleDictionary/MultiKeyDictionary.cs
@@ -1,53 +1,22 @@
-using System;
-using System.Collections.Generic;
+// Задача: Перегрузка индексаторов — MultiKeyDictionary
+// Реализуйте два индексатора: по ключу и по паре (category, key).
+// При чтении отсутствующих ключей кидайте KeyNotFoundException.
 
-namespace App.IndexerOverloading.Task1_SimpleDictionary
+namespace App.IndexerOverloading.Task1_SimpleDictionary;
+
+public class MultiKeyDictionary
 {
-    public class MultiKeyDictionary
+    // Индексатор по одному ключу
+    public int this[string key]
     {
-        private readonly Dictionary<string, int> _simpleDictionary;
-        private readonly Dictionary<(string category, string key), int> _compositeDictionary;
+        get => throw new NotImplementedException();
+        set => throw new NotImplementedException();
+    }
 
-        public MultiKeyDictionary()
-        {
-            _simpleDictionary = new Dictionary<string, int>();
-            _compositeDictionary = new Dictionary<(string category, string key), int>();
-        }
-
-        // Индексатор по строковому ключу
-        public int this[string key]
-        {
-            get
-            {
-                if (_simpleDictionary.TryGetValue(key, out int value))
-                {
-                    return value;
-                }
-                throw new KeyNotFoundException($"Key '{key}' not found in the dictionary.");
-            }
-            set
-            {
-                _simpleDictionary[key] = value;
-            }
-        }
-
-        // Индексатор по составному ключу (два строковых параметра)
-        public int this[string category, string key]
-        {
-            get
-            {
-                var compositeKey = (category, key);
-                if (_compositeDictionary.TryGetValue(compositeKey, out int value))
-                {
-                    return value;
-                }
-                throw new KeyNotFoundException($"Composite key ('{category}', '{key}') not found in the dictionary.");
-            }
-            set
-            {
-                var compositeKey = (category, key);
-                _compositeDictionary[compositeKey] = value;
-            }
-        }
+    // Индексатор по категории и ключу
+    public int this[string category, string key]
+    {
+        get => throw new NotImplementedException();
+        set => throw new NotImplementedException();
     }
 }
