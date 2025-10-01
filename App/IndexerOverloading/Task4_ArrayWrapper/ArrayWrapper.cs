2f8cae597be83ece082e5c6ad7e5abb48c7bcc63
user1 <user1@DESKTOP-UNR49SD>
2025-10-01 11:20:05 +0300

    Task4_ArrayWrapper

--- App/IndexerOverloading/Task4_ArrayWrapper/ArrayWrapper.cs
+++ App/IndexerOverloading/Task4_ArrayWrapper/ArrayWrapper.cs
@@ -1,27 +1,53 @@
-// Задача: Перегрузка индексаторов — ArrayWrapper<T>
-// Реализуйте класс ArrayWrapper<T> согласно описанию в README.
-
 using System;
 
-namespace App.IndexerOverloading.Task4_ArrayWrapper;
-
-public class ArrayWrapper<T>
+namespace App.IndexerOverloading.Task4_ArrayWrapper
 {
-    private readonly T[] _items;
-
-    // Конструктор принимает массив; при null кидает ArgumentNullException
-    public ArrayWrapper(T[] items)
+    public class ArrayWrapper<T>
     {
-        throw new NotImplementedException();
-    }
+        private readonly T[] _array;
 
-    // Длина массива
-    public int Length => throw new NotImplementedException();
+        // Конструктор принимает массив. При null — ArgumentNullException.
+        public ArrayWrapper(T[] array)
+        {
+            _array = array ?? throw new ArgumentNullException(nameof(array), "Array cannot be null");
+        }
 
-    // Индексатор для чтения/записи с проверкой границ
-    public T this[int index]
-    {
-        get => throw new NotImplementedException();
-        set => throw new NotImplementedException();
+        // Свойство Length для получения длины
+        public int Length => _array.Length;
+
+        // Индексатор [int index] для чтения/записи с проверкой границ (IndexOutOfRangeException)
+        public T this[int index]
+        {
+            get
+            {
+                ValidateIndex(index);
+                return _array[index];
+            }
+            set
+            {
+                ValidateIndex(index);
+                _array[index] = value;
+            }
+        }
+
+        // Проверка границ индекса
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= _array.Length)
+            {
+                throw new IndexOutOfRangeException($"Index {index} is out of range. Array length is {_array.Length}");
+            }
+        }
+
+        // Дополнительные полезные методы (опционально)
+        public T[] ToArray()
+        {
+            return (T[])_array.Clone();
+        }
+
+        public bool Contains(T item)
+        {
+            return Array.IndexOf(_array, item) >= 0;
+        }
     }
 }
